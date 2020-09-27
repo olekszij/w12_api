8f2db38abe3c06812fc890bbd210a33ce4d408a2
felizkatus <felizkatus@gmail.com>
2020-09-27 19:09:28 +0200

    POST method improvements

--- server/server.js
+++ server/server.js
@@ -70,53 +70,43 @@ server.get('/api/v1/users', async (req, res) => {
   res.json(users)
 }) // Read
 
+// server.post('/api/v1/users', async (req, res) => {
+//   // Получаем нового пользователя
+//   const newUser = req.body
+//   // Получаем массив из файла users.json функцией readFile()
+//   let users = await readUsersFile()
+//   // Находим максимальный id и присваиваем новому пользователю следующий id
+//   let maxId = 0
+//   users.map((rec) => {
+//     if (maxId < rec.id) maxId = rec.id
+//     return maxId
+//   })
+//   newUser.id = maxId + 1
+//   // Добавляем элемент в массив с помощью оператора spread или функции concat()
+//   users = [...users, newUser]
+//   // Сохраняем файл функцией saveFile(), передав новый массив
+//   saveUsersFile(users)
+//   // Возврадаем статус
+//   res.json({ status: 'SUCCESS', id: newUser.id })
+// }) // Read/Write
+
 server.post('/api/v1/users', async (req, res) => {
   // Получаем нового пользователя
   const newUser = req.body
   // Получаем массив из файла users.json функцией readFile()
-  let users = await readUsersFile()
+  const users = await readUsersFile()
   // Находим максимальный id и присваиваем новому пользователю следующий id
   let maxId = 0
-  users.map((rec) => {
-    if (maxId < rec.id) maxId = rec.id
-    return maxId
+  users.forEach((user) => {
+    if (maxId < user.id) maxId = user.id
   })
   newUser.id = maxId + 1
   // Добавляем элемент в массив с помощью оператора spread или функции concat()
-  users = [...users, newUser]
-  // Сохраняем файл функцией saveFile(), передав новый массив
-  saveUsersFile(users)
+  // и сохраняем файл функцией saveFile()
+  saveUsersFile([...users, newUser])
   // Возврадаем статус
   res.json({ status: 'SUCCESS', id: newUser.id })
 }) // Read/Write
-
-// server.patch('/api/v1/users/:userId', async (req, res) => {
-//   // Получаем нового пользователя
-//   const newData = req.body
-//   // Получаем ID пользователя
-//   const { userId } = req.params
-//   // Получаем массив из файла users.json функцией readFile()
-//   const users = await readUsersFile()
-//   // Методом map() или reduce() перебираем элементы массива.
-//   // Если ID элемента соответствует полученному ID - обновляем элемент,
-//   // в противном случае оставляем исходный элемент.
-//   // Для проверки элемента можно воспользоваться условием:
-//   // rec.id === +userId
-//   users.map((rec) => {
-//     if (rec.id === +userId) {
-//       Object.keys(newData).map((field) => {
-//         // eslint-disable-next-line no-param-reassign
-//         rec[field] = newData[field]
-//         return rec
-//       })
-//     }
-//     return users
-//   })
-//   // Сохраняем файл функцией saveFile(), передав новый массив
-//   saveUsersFile(users)
-//   // Возврадаем статус
-//   res.json({ status: 'SUCCESS', id: userId })
-// }) // Read/Write
 
 server.patch('/api/v1/users/:userId', async (req, res) => {
   // Получаем нового пользователя
